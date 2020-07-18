41260fe9ad08d97328f3153e95d7ba2741f7795c
sunzw <1603309331@qq.com>
2020-07-18 08:53:58 +0800

    更新了packagejson

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,11 +1,31 @@
+// module.exports = {
+//     plugins: {
+//       'autoprefixer': {
+//         browsers: ['Android >= 4.0', 'iOS >= 7']
+//       },
+//       'postcss-pxtorem': {
+//         rootValue: 375,//结果为：设计稿元素尺寸/16，比如元素宽320px,最终页面会换算成 20rem
+//         propList: ['*']
+//       }
+//     }
+//   }
+
+
 module.exports = {
-    plugins: {
-      'autoprefixer': {
-        browsers: ['Android >= 4.0', 'iOS >= 7']
-      },
-      'postcss-pxtorem': {
-        rootValue: 375,//结果为：设计稿元素尺寸/16，比如元素宽320px,最终页面会换算成 20rem
-        propList: ['*']
-      }
+  plugins: {
+    'autoprefixer': {
+      overrideBrowserslist: [
+        "Android 4.1",
+        "iOS 7.1",
+        "Chrome > 31",
+        "ff > 31",
+        "ie >= 8"
+      ]
+    },
+    'postcss-pxtorem': {
+      rootValue: 375,
+      propList: ['*']
     }
   }
+}
+
